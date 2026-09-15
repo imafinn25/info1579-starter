b0f52fbe85dae58f089af18d46bd037c46039434
imafinn25 <60320226+imafinn25@users.noreply.github.com>
2026-09-15 17:18:04 +0000

    change

--- script.js
+++ script.js
@@ -18,22 +18,22 @@ const display = (label, value) =>
 // ADD YOUR CODE BELOW
 
 // TODO: Create variables for your name (string), total number of modules for our class (number), and if you're enrolled (boolean)
-let name = 'Tania';
-let totalModules = 10;
-let isEnrolled = true;
+const myName = "Tania";
+const totalModules = 10;
+const isEnrolled = true;
 
 // TODO: Use a template literal to output a welcome message. Use at least one ${}.
-let message = "Welcome, ${name}!";
+let msg = `Welcome, ${myName}!`
 
 // TODO: Calculate the total study hours for the course. There are 10 modules. Each module takes roughly 6 hours.
 // Formula: totalStudyHours = totalModules * hoursPerWeek
-let hoursPerWeek = 6;
-let totalStudyHours = totalModules * hoursPerWeek;
+const hoursPerWeek = 6;
+const totalStudyHours = totalModules * hoursPerWeek;
 
 // TODO: Calculate the number of study hours each day. Convert the output to minutes (this formula is not provided).
 // Formula: dailyStudyHours = hoursPerWeek / 7
-let dailyStudyHours = hoursPerWeek / 7;
-let dailyStudyMin = dailyStudyHours * 60;
+const dailyStudyHours = hoursPerWeek / 7;
+const dailyStudyMin = dailyStudyHours * 60;
 
 // TODO: Give yourself a rest day and exclude one day out of your week. Calculate the new number of hours and set it to adjustedDailyHours. Convert the output to minutes (this formula is not provided).
 
@@ -44,9 +44,9 @@ let dailyStudyMin = dailyStudyHours * 60;
 
 // TODO: Display your results. Use the correct variables and avoid hard-coding the data below.
 // TODO: Adjust all decimals to two places.
-display("Welcome Message", message);
-display("My Name", name);
-display("Enrolled");
+display("Welcome Message", msg);
+display("My Name", myName);
+display("Enrolled", isEnrolled);
 display("Total Modules");
 display("Daily Study Hours (7 days)");
 display("Daily Study Minutes (7 days)");
